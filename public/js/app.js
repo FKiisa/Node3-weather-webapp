38fa1aa2c81b783a858e042dd814e5b328245cd7
Frank Kiisa <frank.kiisa@derivco.ee>
2019-12-19 13:07:57 +0200

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -14,7 +14,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.innerHTML = '';
     messageThree.innerHTML = '';
     messageFour.innerHTML = '';
-    fetch('http://localhost:3000/weather?address=' + location).then((res) => {
+    fetch('/weather?address=' + location).then((res) => {
         res.json().then((data) => {
             if(data.error) {
                 messageOne.innerHTML = data.error;
